407f46a4338ac44ed5a50770a73b565285bc48e6
Alejandro Fidanza <mercuriodevelop@gmail.com>
2025-11-14 00:31:41 -0500

    add button

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,53 +3,54 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Link from "next/link";
 import { homePath, ticketsPath } from "@/app/paths";
+import { buttonVariants } from "@/components/ui/button";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
-  subsets: ["latin"],
+  subsets: ["latin"]
 });
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
-  subsets: ["latin"],
+  subsets: ["latin"]
 });
 
 export const metadata: Metadata = {
   title: "The Road Next",
-  description: "My Road to Next application...",
+  description: "My Road to Next application..."
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
+                                     children
+                                   }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-      <nav className="
+    <body
+      className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+    >
+    <nav className="
         supports-backdrop-blur:bg-background/60
         fixed left-0 right-0 top-0 z-20
         border-b bg-background/95 backdrop-blur
         w-full flex py-2.5 px-5 justify-between">
-        <div>
-          <Link href={homePath()} className="text-lg font-bold">Home</Link>
-        </div>
-        <div>
-          <Link href={ticketsPath()} className="text-sm underline">Tickets</Link>
-        </div>
-      </nav>
-      <main className="
+      <div>
+          <Link href={homePath()} className={buttonVariants({variant: "outline"})}>Home</Link>
+      </div>
+      <div>
+        <Link href={ticketsPath()} className={buttonVariants({variant: "outline"})}>Tickets</Link>
+      </div>
+    </nav>
+    <main className="
         min-h-screen flex-1
         overflow-y-auto overflow-x-hidden
         py-24 px-8
         bg-secondary/20
         flex flex-co
       ">
-        {children}</main>
-      </body>
+      {children}</main>
+    </body>
     </html>
   );
 }
